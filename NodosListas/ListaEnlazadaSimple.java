50e647d9bf5ff3b04af056be64ae6706c4846bc8
Sebastián Cabezas <sebastian.cabezas.r@gmail.com>
2026-03-24 16:04:57 -0300

    Lista Simplemente Enlazada

--- NodosListas/ListaEnlazadaSimple.java
+++ NodosListas/ListaEnlazadaSimple.java
@@ -4,15 +4,30 @@ public class ListaEnlazadaSimple {
     
     private NodoEnteros head;
 
+    // Constructor por defecto
     public ListaEnlazadaSimple(){
         this.head = null;
     }
 
     public void mostrar(){
-        NodoEnteros aux = head;
-        while (aux != null) {
-            aux.toString();
+        if (this.listaVacia()) {
+            System.out.println("--- Lista Vacia ---");
+        }else{
+            NodoEnteros aux = head;
+            while (aux != null) {
+                System.out.println(aux.toString());
+            }
         }
+    }
+
+    public boolean listaVacia(){
+        return head == null;
+    }
+
+    public static void main(String[] args) {
+        ListaEnlazadaSimple les = new ListaEnlazadaSimple();
+        les.mostrar();
+        
     }
 
 }
