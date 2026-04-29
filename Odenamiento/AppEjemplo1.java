0b8a57007377a38f63d6261c15f37cf508974926
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-04-29 08:50:51 -0400

    remoto1.2: biblioteca + ejemplo

--- Odenamiento/AppEjemplo1.java
+++ Odenamiento/AppEjemplo1.java
@@ -7,12 +7,21 @@ public class AppEjemplo1 {
     public static void main(String[] args) {
         Biblioteca lib = new Biblioteca();
         // arreglo de 5 numeros enteros
-        int [] numeros = {50, 10, 35, 80, 5};
+        int[] numeros = { 50, 10, 35, 80, 5 };
         // System.out.println("Arreglo: " + Arrays.toString(numeros));
         lib.printArregloEntero(numeros, "Original");
 
         // Paso 1: Ordenar el arreglo
         Arrays.sort(numeros);
         lib.printArregloEntero(numeros, "Ordenado");
+
+        // Paso 2: Buscar
+        int buscado = 35;
+        int posicionBuscado = Arrays.binarySearch(numeros, buscado);
+        if (posicionBuscado < 0) {
+            System.out.println("Error 404: El " + buscado + " no fue encontrado.");
+        } else {
+            System.out.println("El " + buscado + " está en el índice [" + posicionBuscado + "]");
+        }
     }
 }
