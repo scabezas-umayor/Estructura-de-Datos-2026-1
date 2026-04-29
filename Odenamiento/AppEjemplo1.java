bec312cf61091f8f6a8fd6a36fd9adcbab8a09e0
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-04-29 08:39:43 -0400

    remoto1.1: biblioteca + ejemplo

--- Odenamiento/AppEjemplo1.java
+++ Odenamiento/AppEjemplo1.java
@@ -1,10 +1,18 @@
 package Odenamiento;
 
+import java.util.Arrays;
+
 public class AppEjemplo1 {
     // psvm
     public static void main(String[] args) {
+        Biblioteca lib = new Biblioteca();
         // arreglo de 5 numeros enteros
         int [] numeros = {50, 10, 35, 80, 5};
-        
+        // System.out.println("Arreglo: " + Arrays.toString(numeros));
+        lib.printArregloEntero(numeros, "Original");
+
+        // Paso 1: Ordenar el arreglo
+        Arrays.sort(numeros);
+        lib.printArregloEntero(numeros, "Ordenado");
     }
 }
--- Odenamiento/Biblioteca.java
+++ Odenamiento/Biblioteca.java
@@ -1,13 +1,25 @@
 package Odenamiento;
 
+import java.util.Arrays;
+
 public class Biblioteca {
     
     public void printArregloEntero(int [] _arr, String _titulo){
         System.out.println("=== " + _titulo + " ===");
+        // Con Bibliotecas
+        System.out.println(Arrays.toString(_arr));
+        // Sin Bibliotecas
+        /*
+        System.out.print("[");
         for (int i = 0; i < _arr.length; i++) {
-            System.out.print(_arr[i] + " ");
+            System.out.print(_arr[i]);
+            // System.out.print(_arr[i] + ", ");
+            if (i != _arr.length -1){
+                System.out.print(", ");
+            }
         }
-        System.out.println();
+        System.out.println("]");
+        */
     }
 
 }
