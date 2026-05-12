45cf4641addff91c704e6cd79c68ee2b2af57e70
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-05-12 14:29:55 -0400

    Arboles

--- Arboles/ArbolBinario.java
+++ Arboles/ArbolBinario.java
@@ -8,4 +8,24 @@ public class ArbolBinario {
         this.raiz = null;
     }
 
+    // 1. Inserción recursiva
+    public void insertar(int _dato) {
+        this.raiz = insertarRecursivo(this.raiz, _dato);
+    }
+
+    private NodoArbol insertarRecursivo(NodoArbol _actual, int _dato){
+
+        if (_actual == null) {
+            return new NodoArbol(_dato);
+        }
+
+        if (_dato < _actual.dato) {
+            _actual.izquierdo = insertarRecursivo(_actual.izquierdo, _dato);
+        } else if (_dato > _actual.dato) {
+            _actual.derecho = insertarRecursivo(_actual.derecho, _dato);
+        }
+
+        return _actual;
+    }
+
 }
