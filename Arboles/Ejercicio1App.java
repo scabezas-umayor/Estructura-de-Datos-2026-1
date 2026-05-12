b936cf7597b06912cf96096b38b285c2c63468e3
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-05-12 15:11:09 -0400

    app ejercicio 1

--- Arboles/Ejercicio1App.java
+++ Arboles/Ejercicio1App.java
@@ -1,7 +1,7 @@
 package Arboles;
 
 public class Ejercicio1App {
-    
+
     public static void main(String[] args) {
         // insertar los siguientes valores:
         // 50, 30, 70, 20, 40, 60 y 80
@@ -9,12 +9,17 @@ public class Ejercicio1App {
         ab.insertar(50);
         ab.insertar(30);
         ab.insertar(70);
+        ab.insertar(20);
+        ab.insertar(40);
+        ab.insertar(60);
+        ab.insertar(80);
         System.out.println("Recorrido InOrden");
         ab.recorridoInOrden(ab.getRaiz());
         System.out.println("\nRecorrido PreOrden");
         ab.recorridoPreOrden(ab.getRaiz());
         System.out.println("\nRecorrido PostOrden");
         ab.recorridoPostOrden(ab.getRaiz());
+        // miercoles: hacer el recorrido desde un arreglo con los valores iniciales
     }
 
 }
