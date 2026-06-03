524426bde457d18c691ffdf4957ec60c4413f21a
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-06-03 08:37:39 -0400

    tablas hash: insertar

--- TablasHashing/TablaHashManual.java
+++ TablasHashing/TablaHashManual.java
@@ -14,5 +14,21 @@ public class TablaHashManual {
         return Math.abs(_clave) % this.tam;
     }
 
-    
+    public void insertar(int _clave, String _valor) {
+        int indice = funcionHash(_clave);
+        NodoHashManual nuevo = new NodoHashManual(_clave, _valor);
+
+        // Si la casilla está vacía, entonces el nuevo nodo es el primero
+        if (this.tabla[indice] == null) {
+            this.tabla[indice] = nuevo;
+        } else {
+            // Si hay colisión, pq si hay, entonces recorremos manualmente la lista hasta el
+            // final
+            NodoHashManual aux = this.tabla[indice];
+            while (aux.siguiente != null) {
+                aux = aux.siguiente;
+            }
+            aux.siguiente = nuevo;
+        }
+    }
 }
