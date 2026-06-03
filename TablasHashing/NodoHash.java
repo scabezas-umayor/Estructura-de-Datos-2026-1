154fc5b6844170cf0e71807ee4eb82daa07008ac
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-06-03 09:11:41 -0400

    tablas hash: nodo hash

---
+++ TablasHashing/NodoHash.java
@@ -0,0 +1,11 @@
+package TablasHashing;
+
+public class NodoHash {
+    int clave;
+    String valor;
+
+    public NodoHash(int _clave, String _valor) {
+        this.clave = _clave;
+        this.valor = _valor;
+    }
+}
