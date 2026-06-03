7502b1029971a078adcd6c513213012fa06c9d1b
Sebastián Cabezas Ríos <scabezas@zoidberg.local>
2026-06-03 09:24:38 -0400

    app2: leer con scanner

---
+++ TablasHashing/App2.java
@@ -0,0 +1,17 @@
+package TablasHashing;
+
+import java.util.Scanner;
+
+public class App2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese su nombre: ");
+        String nombre = leer.nextLine();
+        System.out.println("Su nombre es " + nombre);
+
+
+    }
+}
